be35a855b4827e72859ce4d7df9cfa2c3ed12a24
Sajal Gupta <gupta.sajal631@gmail.com>
2022-01-09 19:35:59 +0530

    optimzation

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,6 @@
 window.addEventListener("load", event => {
-    var image = document.querySelectorAll('.banner_container');
-    for (i = 0; i < image.length; i++){
-        image[i].classList.remove('loading');
+    var loading = document.querySelectorAll('.loading');
+    for (i = 0; i < loading.length; i++){
+        loading[i].classList.remove('loading');
     }
-});
-
-window.addEventListener("load", event => {
-    var image = document.querySelector('.card_profile');
-    image.classList.remove('loading');
 });
