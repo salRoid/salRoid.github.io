ffbbb21fefc08746bd4272666e774107bb3ebca2
Sajal Gupta <gupta.sajal631@gmail.com>
2023-05-04 12:48:06 +0530

    Audio Changes

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,10 @@
 const audio = document.getElementById("aud");
 
 audio.addEventListener("canplaythrough", () => {
-    audio.play().catch(e => {
-       window.addEventListener('click', () => {
-          audio.play();
-          $('.play_icon').hide();
-       })
-    })
-   });
-
-function playAudio() {
-   $('.play_icon').show();
-   $('.mute_icon').hide();
-   audio.pause();
-}
-
-function pauseAudio() {
-   $('.mute_icon').show();
-   $('.play_icon').hide();
-   audio.play();
-}
+   audio.play().catch(e => {
+      window.addEventListener('click', () => {
+         audio.play();
+         $('.play_icon').hide();
+      })
+   })
+});
--- js/sliderVideo.js
+++ js/sliderVideo.js
@@ -3,33 +3,10 @@ const audio = document.getElementById("aud");
 audio.addEventListener("canplaythrough", () => {
     audio.play().catch(e => {
        window.addEventListener('click', () => {
-         $('.play_icon').css('display', 'none');
-         $('.mute_icon').css('display', 'flex');
          audio.play();
        })
     })
    });
-
-var playButton = document.getElementById("play_icon");
-playButton.addEventListener('click', () => {
-   console.log($('.mute_icon'));
-   console.log($('.play_icon'));
-   $('.mute_icon').css('display', 'flex');
-   $('.play_icon').css('display', 'none');
-   console.log('Play the audio', audio);
-   audio.play();
-});
-
-
-var muteButton = document.getElementById("mute_icon");
-muteButton.addEventListener('click',  () => {
-   console.log($('.mute_icon'));
-   console.log($('.play_icon'));
-   $('.play_icon').css('display', 'flex');
-   $('.mute_icon').css('display', 'none');
-   console.log('Pause the audio', audio);
-   audio.pause();
-});
 
 var myCarousel = document.getElementById('weddingCarousel')
 myCarousel.addEventListener('slide.bs.carousel', function () {
@@ -40,3 +17,17 @@ myCarousel.addEventListener('slide.bs.carousel', function () {
       vids[0].play();
    }
 });
+
+// var playButton = document.getElementById("play_icon");
+// playButton.addEventListener('click', () => {
+//    $('.mute_icon').css('display', 'flex');
+//    $('.play_icon').css('display', 'none');
+//    audio.play();
+// });
+
+// var muteButton = document.getElementById("mute_icon");
+// muteButton.addEventListener('click',  () => {
+//    $('.play_icon').css('display', 'flex');
+//    $('.mute_icon').css('display', 'none');
+//    audio.pause();
+// });
